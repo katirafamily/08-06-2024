14fef0a8bdd8ea3995433fd89a78655c9bf127f1
katirafamily <katirafamily>
2024-06-08 19:31:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// for(var i=0; i<=10; i++){
+//     for(var j=1; j<=5; j++){
+//       console.log(i,j)
+//     }
+// }
+var name = ["nabiha", "mahnoor", "hassan", "emaan"];
+var fatherName = "Mzameer-khan";
+
+for (var i = 0; i < name.length; i++) {
+  console.log(name[]);
+}
+
+
